92eb72f7add18442380947097f59a1bdb46c34b6
jasonuc <mrjasonu@gmail.com>
2024-02-15 21:23:42 +0000

    Refactor Password component to fix formatting

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,5 +1,5 @@
 
-function Password({password}) {
+function Password({ password }) {
     return (
         <div className="flex flex-grow items-center justify-center font-oswald font-extrabold tracking-wider border-t-2 w-full">
             <p className="">{password}</p>
